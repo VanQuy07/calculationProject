7fc4f26c46ecbadf9e5208976d7011d90b7ca8d5
banthantrandan@gmail.com <nguyenvanquy16072005@gmail.com>
2024-10-03 19:48:44 +0700

    first commit

---
+++ mainCalculator.java
@@ -0,0 +1,13 @@
+
+package calculation;
+
+
+
+public class mainCalculator {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+                
+    }
+    
+}
